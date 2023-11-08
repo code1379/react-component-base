54bd6e77d72fdf3ed06b03bf883d9efb68f7c9d4
Coderxia Luo <guojingzhou@huayun.com>
2023-11-08 15:27:09 +0800

    feat: Input 組件受控

--- src/input/Input.stories.tsx
+++ src/input/Input.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import Input from "./index.tsx";
+import { useState } from "react";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -19,11 +20,20 @@ const meta = {
 } satisfies Meta<typeof Input>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+// type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Basic: Story = {
-  args: {
-    children: "content",
-  },
+export const Basic = () => {
+  return <Input value="abc" placeholder="basic usage" />;
+};
+
+export const Control = () => {
+  const [value, setValue] = useState("111");
+
+  const onChange = (e) => {
+    console.log("!!!");
+    console.log("e.target.value", e.target.value);
+    setValue(e.target.value);
+  };
+  return <Input value={value} placeholder="basic usage" onChange={onChange} />;
 };
--- src/input/Input.tsx
+++ src/input/Input.tsx
@@ -1,25 +1,66 @@
 import classNames from "classnames";
 
 import "./index.css";
+import { CSSProperties, ReactNode, useEffect, useState } from "react";
 
-export interface InputProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
+  defaultValue?: string;
+  value?: string;
+  placeholder?: string;
+  onChange?: (event: React.FormEvent<HTMLInputElement>) => void;
   className?: string;
-  children?: React.ReactNode;
-  style?: React.CSSProperties;
+  children?: ReactNode;
+  style?: CSSProperties;
 }
 
 const Input = (props: InputProps) => {
-  const { className, style, children, ...other } = props;
+  const {
+    className,
+    onChange,
+    placeholder,
+    style,
+    children,
+    defaultValue = "",
+    ...other
+  } = props;
 
   const cls = classNames({
     "ant-input": true,
     [className as string]: !!className,
   });
 
+  const [value, setValue] = useState(props.value || defaultValue);
+
+  const handleInputChange = (e) => {
+    // if ("value" in props) return;
+    // const _value = e.target.value;
+    // setValue(_value);
+    // if (typeof onChange === "function") {
+    //   onChange(_value);
+    // }
+
+    const _value = e.target.value;
+    if (!("value" in props)) {
+      setValue(_value);
+    }
+    console.log("????");
+    onChange?.(e);
+  };
+
+  useEffect(() => {
+    if ("value" in props) {
+      setValue(props.value);
+    }
+  }, [props.value]);
+
   return (
-    <div className={cls} style={style} {...other}>
-      {children}
-    </div>
+    <input
+      className={cls}
+      value={value}
+      type="text"
+      placeholder={placeholder}
+      onChange={handleInputChange}
+    />
   );
 };
 
